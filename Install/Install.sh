9e876ac60e73bea5d96b4b99bc9aa6241acd90a7
Filexel <mhehopm34@mail.ru>
2026-06-23 17:02:25 +0500

    Create Install.sh

---
+++ Install/Install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Установка Curse..."
+
+# Скачиваем файлы
+wget https://raw.githubusercontent.com/Filexel/Curse/main/Utils/Exit.sh
+wget https://raw.githubusercontent.com/Filexel/Curse/main/Utils/Start.sh
+
+# Копируем в bin
+cp Start.sh $PREFIX/bin/curse-start
+cp Exit.sh $PREFIX/bin/curse-exit
+
+# Даём права на выполнение
+chmod +x $PREFIX/bin/curse-start
+chmod +x $PREFIX/bin/curse-exit
+
+echo "Установка завершена!"
+echo "curse-start — Запустить Curse Shell"
+echo "curse-exit — Выйти"
+
+
+1.0
