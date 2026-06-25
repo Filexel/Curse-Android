1bb6123a10f0fee30a0b8129094d3897aad02383
Filexel <mhehopm34@mail.ru>
2026-06-25 13:39:10 +0500

    Update Download.sh

--- Install/Download.sh
+++ Install/Download.sh
@@ -3,7 +3,7 @@
 os=$(uname -o)
 echo "OS: $os"
 sleep 1
-wget https://raw.githubusercontent.com/Filexel/Curse/refs/heads/main/Install/Install.sh
+wget https://raw.githubusercontent.com/Filexel/Curse-Android/refs/heads/main/Install/Install.sh
 
 chmod +x Install.sh
 apt update
@@ -12,4 +12,4 @@ apt clean
 
 ./Install.sh
 
-1.0
+2.0
